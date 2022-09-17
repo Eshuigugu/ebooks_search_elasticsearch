e316ae7d78420cf82e19df132c81bccb93fa0207
Eshuigugu <65567073+Eshuigugu@users.noreply.github.com>
2022-09-16 23:06:41 -0700

    Update main.py

--- main.py
+++ main.py
@@ -116,7 +116,7 @@ def split_str_to_set(strings):
 
 
 def search_elasticsearch(title, authors):
-    query_str = f'{reduce_title(title)} {reduce_author_str(authors[0])}'
+    query_str = f'{reduce_title(title)} {reduce_author_str(authors[0]) if authors else ""}'
     hits = query_elasticsearch(title=title, authors=' '.join(authors))
     hits += search_calishot(query_str) + search_openlibrary(query_str)
     if hits:
